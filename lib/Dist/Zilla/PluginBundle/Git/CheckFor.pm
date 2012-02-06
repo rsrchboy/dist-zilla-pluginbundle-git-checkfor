b52a13ea7c7697baa817a9282c492f5d1157ffc7
Chris Weyl <cweyl@alumni.drew.edu>
2012-02-06 12:28:09 -0800

    initial pluginbundle

--- lib/Dist/Zilla/PluginBundle/Git/CheckFor.pm
+++ lib/Dist/Zilla/PluginBundle/Git/CheckFor.pm
@@ -1,11 +1,24 @@
 package Dist::Zilla::PluginBundle::Git::CheckFor;
 
-# ABSTRACT: The great new Dist::Zilla::PluginBundle::Git::CheckFor!
+# ABSTRACT: All Git::CheckFor plugins at once
 
 use Moose;
 use namespace::autoclean;
 
-# XXX
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+
+sub configure {
+    my ($self) = @_;
+
+    $self->add_plugins(
+        [ 'Git::CheckFor::CorrectBranch' => $self->config_slice('release_branch') ],
+        'Git::CheckFor::Fixups',
+    );
+
+    return;
+}
+
+
 
 __PACKAGE__->meta->make_immutable;
 
@@ -13,11 +26,24 @@ __PACKAGE__->meta->make_immutable;
 
 __END__
 
+=for Pod::Coverage configure
+
 =head1 SYNOPSIS
+
+    ; in dist.ini
+    [@Git::CheckFor]
 
 =head1 DESCRIPTION
 
+This bundles several plugins that do some sanity/lint checking of your git
+repository; namely: you're on the right branch and you haven't forgotten any
+autosquash commits (C<fixup!> or C<squash!>).
+
 =head1 SEE ALSO
+
+L<Dist::Zilla::Plugin::Git::CheckFor::Fixups>
+
+L<Dist::Zilla::Plugin::Git::CheckFor::CorrectBranch>
 
 L<Dist::Zilla::PluginBundle::Git>
 
