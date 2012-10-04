fd47c6c08dc06c74f0e565f49e5452d7131df459
Chris Weyl <cweyl@alumni.drew.edu>
2012-10-03 21:34:39 -0700

    Pull over code from Git::NextVersion that we depend on
    This was dropped from the latest releases, and unfortunately, we depend on it.
    I'm pulling it in here until a more permanent solution can be found.

--- lib/Dist/Zilla/Role/Git/Repo/More.pm
+++ lib/Dist/Zilla/Role/Git/Repo/More.pm
@@ -13,8 +13,65 @@ with
     ;
 
 has _repo => (is => 'lazy', isa => 'Git::Wrapper');
-
 sub _build__repo { Git::Wrapper->new(shift->repo_root) }
+
+
+# -- attributes
+
+has version_regexp => ( is => 'rwp', isa=>'Str', default => '^v(.+)$' );
+has first_version  => ( is => 'rwp', isa=>'Str', default => '0.001' );
+
+has _previous_versions => (
+
+    traits  => ['Array'],
+    is      => 'lazy',
+    isa     => 'ArrayRef[Str]',
+    handles => {
+
+        has_previous_versions => 'count',
+        previous_versions     => 'elements',
+        earliest_version      => [ get =>  0 ],
+        last_version          => [ get => -1 ],
+    },
+);
+
+sub _build__previous_versions {
+  my ($self) = @_;
+
+  local $/ = "\n"; # Force record separator to be single newline
+
+  my $git  = Git::Wrapper->new( $self->repo_root );
+  my $regexp = $self->version_regexp;
+
+  my @tags = $git->tag;
+  @tags = map { /$regexp/ ? $1 : () } @tags;
+
+  # find tagged versions; sort least to greatest
+  my @versions =
+    sort { version->parse($a) <=> version->parse($b) }
+    grep { eval { version->parse($_) }  }
+    @tags;
+
+  return [ @versions ];
+}
+
+# -- role implementation
+
+#sub provide_version {
+  #my ($self) = @_;
+
+  ## override (or maybe needed to initialize)
+  #return $ENV{V} if exists $ENV{V};
+
+  #return $self->first_version
+    #unless $self->has_previous_versions;
+
+  #my $last_ver = $self->last_version;
+  #my $new_ver  = Version::Next::next_version($last_ver);
+  #$self->log("Bumping version from $last_ver to $new_ver");
+
+  #return "$new_ver";
+#}
 
 !!42;
 
